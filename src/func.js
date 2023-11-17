d6749175f4428f6d0f7924336e8ec1df8c37fd65
Rick van Laarhoven <hello@rickvanlaarhoven.nl>
2023-11-17 10:38:29 +0100

    fix: Disable autodetect and format eslint

--- src/func.js
+++ src/func.js
@@ -5,31 +5,32 @@ Contributors: Garth Bjerk <garth@bakkenbaeck.no>,
 Description: A high-level language FunC is used to program smart contracts on TON.
 Website: https://docs.ton.org/develop/func/overview
 */
-module.exports = function (hljs) {
+module.exports = function(hljs) {
   return {
     name: "FunC",
     keywords: {
       type: "int slice cell builder tuple cont",
       keyword:
         "null asm forall impure inline inline_ref method_id var if return throw_unless do until while ifnot",
-      built_in: "main recv_internal recv_external run_ticktock",
+      built_in: "main recv_internal recv_external run_ticktock"
     },
     contains: [
       hljs.C_NUMBER_MODE,
       {
         scope: "string",
         begin: '"',
-        end: '"',
+        end: '"'
       },
       hljs.COMMENT(
         "{-", // begin
         "-}" // end
       ),
-      hljs.COMMENT(";;", '$'),
-	  {
-		className: "symbol",
-		begin: /\+|-|:|&|=|{|}|;|,|\/|\*|%|\^|\[|\]|<|>|\||~|\(|\)/
-	  },
+      hljs.COMMENT(";;", "$"),
+      {
+        className: "symbol",
+        begin: /\+|-|:|&|=|{|}|;|,|\/|\*|%|\^|\[|\]|<|>|\||~|\(|\)/
+      }
     ],
+    disableAutodetect: true
   };
 };
